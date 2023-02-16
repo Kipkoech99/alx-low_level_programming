159852bd57d63199a793dffe6d888ba8d7613fc8
Kipkoech99 <kipkoechevans99@gmail.com>
2023-02-16 10:37:00 -0800

    UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,14 +9,7 @@
 int main(void)
 
 {
-	const char message [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	const unsigned int message _len = sizeof(mesage) - 1;
-
-	ssize_t bytes_written = write(STDERR_FILENO, message, message_len);
-	if (bytes_written != message_len);
-		return 1;
-	
-		
-	return 1;
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59");
+	return (1);
 
 }
