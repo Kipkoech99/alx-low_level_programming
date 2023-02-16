8fdac63cc45fcd312c63b04a71596f05635873f1
Kipkoech99 <kipkoechevans99@gmail.com>
2023-02-16 06:04:56 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly with proper grammar, but the outcome is a piece of art,
+ * main - prints exactly with proper grammar, but the outcome is a piece
  * You are not allowed to use the function puts
  * Return: Always 0 (success)
  */
