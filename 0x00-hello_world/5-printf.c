92db1fb7d97204fc72cd686742548a4cd6026500
Kipkoech99 <kipkoechevans99@gmail.com>
2023-02-16 05:58:48 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* main: prints exactly with proper grammar, but the outcome is a piece of art,
+ * You are not allowed to use the function puts
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art\,");
+
+	return (0);
+
+}
