286bbd948e19f54f8a4113499005bcd49f3fe90f
Kipkoech99 <kipkoechevans99@gmail.com>
2023-02-16 05:07:30 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(void) /* Hello, puts */
+/**
+ * main - Entry point of the program
+ *
+ * Return : Always 0 (success)
+ */
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
