620944ae25a7eb5936f36cfa6a5f03e1a0a8664f
Kipkoech99 <kipkoechevans99@gmail.com>
2023-02-16 05:30:33 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+
 /**
  * main - Entry point of the program
  *
  * Return : Always 0 (success)
  */
-int main(void) {
 
-	puts('"Programming is like building a multilingual puzzle\n');
-	return (0);
+
+int main(void) 
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
 }
