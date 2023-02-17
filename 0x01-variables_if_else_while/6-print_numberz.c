e13a4de265333d006ff325ce20afe2beec7d0505
Kipkoech99 <kipkoechevans99@gmail.com>
2023-02-17 08:32:39 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdllib.h>
+#include <time.h>
+
+/**
+ * main - Entry point for this program
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n')
+	
+	return (0);
+
+}
