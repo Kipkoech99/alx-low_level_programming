b83949c5b485690c1e2c12eb6eab265b17945e04
Kipkoech99 <kipkoechevans99@gmail.com>
2023-02-17 07:59:32 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point for this program
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	prinf("%d", i);
+	printf("\n");
+
+	return (0);
+
+}
