5a7fb7bd06eeb68c650c8e3150ba1c3995a1f7c4
Kipkoech99 <kipkoechevans99@gmail.com>
2023-03-29 06:05:15 -0700

    Task 3 correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -26,13 +26,13 @@ int _atoi(char *s)
 		len++;
 
 	while (i < len && f == 0)
-		{
-			if (s[i] == '-')
-				++d;
+	{
+		if (s[i] == '-')
+			++d;
 
-			if (s[i] >= '0' && s[i] <= '9')
-			{
-				digit = s[i] - '0';
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
 
 			if (d % 2)
 				digit = -digit;
@@ -42,14 +42,11 @@ int _atoi(char *s)
 
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
+
 			f = 0;
-			}
-			i++;
 		}
-
-	if (f == 0)
-	return (0);
-
+		i++;
+	}
 	return (n);
 }
 /**
@@ -67,15 +64,14 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
 	num1 = _atoi(argv[1]);
 
 	num2 = _atoi(argv[2]);
 
 	result = num1 * num2;
 
-
 	printf("%d\n", result);
 
 	return (0);
 }
+
