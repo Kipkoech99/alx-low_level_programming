d6723df64098488075973ab8a14f2532035a9592
Kipkoech99 <kipkoechevans99@gmail.com>
2023-03-29 03:31:00 -0700

    Task 0 changes

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,6 +10,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
+	unsigned int i;
 
 	va_start(args, n);
 
@@ -19,7 +20,7 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	int sum = 0;
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
