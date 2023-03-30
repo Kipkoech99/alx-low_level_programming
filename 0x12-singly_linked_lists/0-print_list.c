4c7e506f92fded59b64c57cabde0337b60297eae
Kipkoech99 <kipkoechevans99@gmail.com>
2023-03-30 02:43:49 -0700

    My first commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,12 +14,12 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str)
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		else
 			printf("[0] (nill)\n");
 
 		count++;
 		h = h->next;
 	}
-	return count;
+	return (count);
 }
