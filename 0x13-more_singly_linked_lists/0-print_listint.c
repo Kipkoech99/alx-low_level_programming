c462a3c2281707a46b34e7da2c4f7d96aa80e062
Kipkoech99 <kipkoechevans99@gmail.com>
2023-04-04 11:51:59 -0700

    Task 0 undo changes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	listint_t *current = h;
 
-	while (current != NULL)
+	while (h)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		printf("%d\n", h->n);
 		count++;
+		h = h->next;
 	}
 
 	return (count);
