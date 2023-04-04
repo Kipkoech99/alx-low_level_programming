2336a0ff8145106129ed60e47b9a852be97abe86
Kipkoech99 <kipkoechevans99@gmail.com>
2023-04-04 06:33:36 -0700

    Task 7 changes

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,7 +9,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_int *current = head;
-	unsigned int i;
+	unsigned int i = 0;
 
 	while (current && i < index)
 	{
@@ -17,5 +17,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		i++;
 	}
 
-	return (current ? temp: NULL);
+	return (current ? temp : NULL);
 }
