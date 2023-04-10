db3ebb02e3742a080f8491e4e05274c208ffb20a
Kipkoech99 <kipkoechevans99@gmail.com>
2023-04-10 00:03:44 -0700

    Task 0 changes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,51 +3,36 @@
 /**
  * read_textfile - a function that reads a text file
  * and prints it to the POSIX standard output.
+ * @filename: the name of the file
+ * @letters - number of characters
  * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t bytes_num;
-	ssize_t content;
-
 	if (filename == NULL)
 	{
-		return (0);
+		return (-1);
 	}
+	FILE *fp;
 
-	FILE *txtfile = fopen(filename, "r");
-	if (txtfile == NULL)
+	fp = fopen("filename", "r");
+	if (fp == NULL)
 	{
-		return (0);
+		return (-1);
 	}
+	buffer = malloc(sizeof(char) * letters);
 
-	char *mem = (char *) malloc(letters + 1);
-	if (mem == NULL)
+	bytes_read = fread(buffer, letters, fp);
+	if (bytes_read == NULL)
 	{
-		return (0);
+		return (-1);
 	}
-	ssize_t bytes_num = 0;
-
-	ssize_t bytes_num = fread(mem, sizeof(char), letters, txtfile);
-	if (bytes == -1)
+	bytes_written = fwrite(bytes_read, buffer, STDOUT_FILENO);
+	if (bytes_written == NULL)
 	{
-		fclose(txtfile);
-		free(mem);
-		return (0);
+		return (-1);
 	}
-	mem[bytes_num] = '\0'
-	
-	ssize_t content = 0;
-
-	ssize_t content = fwrite(mem, sizeof(char), letters, stdout);
-	if (content == -1 || content != bytes_num)
-	{
-		fclose(txtfile);
-		free(mem);
-		return (0);
-	}
-	fclose(txtfile);
-	free(mem);
-
-	return (bytes_num);
+	free(buffer);
+	fclose(fp);
+	return (bytes_written);
 }
