7b002918b04e0f0be472fcf4c59f988cd8af8097
Kipkoech99 <kipkoechevans99@gmail.com>
2023-04-10 02:43:04 -0700

    Task 1 changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	bytes_written = write(fp, text_content, len);
 
-	if (fp == -1 || w == -1)
+	if (fp == -1 || bytes_written == -1)
 		return (-1);
 
 	close(fp);
