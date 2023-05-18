f42ef863b4f006871d7ef808267a42dea4cf5c76
Kipkoech99 <kipkoechevans99@gmail.com>
2023-05-18 14:08:48 -0700

    Task 7 and 8 changes

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,54 +1,54 @@
 #include "lists.h"
 
 /**
- * *insert_dnodeint_at_index - inserts a new node at a given position.
- * @h: pointer
- * @idx: index
- * @n: data
- * Return: address of new node
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = *h;
-	size_t count = 0;
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-
-	if (h == NULL)
-	{
-		return (NULL);
-	}
-
-	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
-
+	new = NULL;
 	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
 	{
-		new->next = *h;
-		if (*h != NULL)
-			(*h)->prev = new;
-		*h = new;
-		return (new);
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
-	while (current != NULL && count < idx)
-	{
-		current = current->next;
-		count++;
-	}
-	if (current == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->next = current;
-	new->prev = current->prev;
-	current->prev->next = new;
-	current->prev = new;
 
 	return (new);
 }
